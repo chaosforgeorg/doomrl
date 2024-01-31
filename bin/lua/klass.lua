d97eb600e4671da6a6f26e73c421b971dd428d19
epyon <epyon@chaosforge.org>
2024-01-31 17:12:30 +0100

    vampyre blocks ironman

--- bin/lua/klass.lua
+++ bin/lua/klass.lua
@@ -26,7 +26,7 @@ function DoomRL.load_klasses()
 			{ id = "triggerhappy", requires = {{ "bitch",      2 }}, max = 2 },
 			{ id = "whizkid",      requires = {{ "finesse",    2 }}, max = 2 },
 
-			{ id = "vampyre",      requires = {{ "berserker",    1 }, { "badass",       1 },               }, blocks = { "eagle",      "bitch",     "hellrunner", }, reqlevel = 6, master = true },
+			{ id = "vampyre",      requires = {{ "berserker",    1 }, { "badass",       1 },               }, blocks = { "ironman",    "bitch",     "hellrunner", }, reqlevel = 6, master = true },
 			{ id = "bulletdance",  requires = {{ "dualgunner",   1 }, { "triggerhappy", 1 },               }, blocks = { "hellrunner", "intuition", "brute",      },               master = true },
 			{ id = "armydead",     requires = {{ "shottyman",    1 }, { "badass",       1 },               }, blocks = { "gun",        "eagle",     "hellrunner", }, reqlevel = 6, master = true },
 			{ id = "ammochain",    requires = {{ "triggerhappy", 2 }, { "reloader",     2 },               }, blocks = { "nails",      "gun",       "eagle",      },               master = true },
