1da3faac6eafe0f4255b49cd50963bc40e2b3ab1
epyon <epyon@chaosforge.org>
2024-11-28 16:10:14 +0100

    EXPERIMENTAL kill counter fix

--- src/dfplayer.pas
+++ src/dfplayer.pas
@@ -403,7 +403,7 @@ procedure TPlayer.RegisterKill ( const aKilledID : AnsiString; aKiller : TBeing;
 var iKillClass : AnsiString;
 begin
   iKillClass := 'other';
-  if aKiller = Self then
+  if ( aKiller = Self ) and ( TLevel(Parent).ActiveBeing = Self ) then
   begin
     iKillClass := 'melee';
     if aWeapon <> nil then
