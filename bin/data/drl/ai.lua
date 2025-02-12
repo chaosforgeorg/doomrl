b7fb44e73b580775f543b5b5ccacabfdf60d5405
epyon <epyon@chaosforge.org>
2025-02-12 17:51:41 +0100

    mastermind AI is done, all AI's are now refactored

--- bin/data/drl/ai.lua
+++ bin/data/drl/ai.lua
@@ -496,131 +496,114 @@ register_ai "mastermind_ai"
 {
 
 	OnCreate = function( self )
-		self:add_property( "ai_state", "thinking" )
-		self:add_property( "assigned", false )
-		self:add_property( "move_to", coord.new(0,0) )
-		self:add_property( "attackchance", math.min( self.__proto.attackchance * diff[DIFFICULTY].speed, 90 ) )
+		aitk.basic_init( self, false, false )
 		self:add_property( "stun_time", 0 )
 		self:add_property( "previous_hp", self.hpmax )
+		self:add_property( "irritated", 0 )
+		self.ai_state = "hunt"
 	end,
 
 	OnAttacked = function( self )
+		if self.stun_time > 0 then return end
 		local damage_taken = self.previous_hp - self.hp
 		if damage_taken >= 20 then
+			self:play_sound( "act" )
 			self.stun_time = math.floor(damage_taken/20)
-			self.assigned = false
 			self:msg("", "The ".. self.name .." flinched!")
+			self.ai_state = "stagger"
 		end
 	end,
 
 	states = {
-		thinking = function( self )
+		stagger = function( self )
+			local direction = generator.random_empty_coord( { EF_NOBEINGS, EF_NOBLOCK }, area.around( self.position ) )
+			self:direct_seek( direction )
+			self.previous_hp = self.hp
+			self.stun_time = self.stun_time - 1
+			if self.stun_time > 0 then
+				return "stagger"
+			else
+				return "hunt"
+			end
+		end,
+
+		hunt = function( self )
 			local dist       = self:distance_to( player )
 			local visible    = self:in_sight( player )
 			self.previous_hp = self.hp
 
-			if visible and self.stun_time == 0 then
+			if math.random(30) == 1 then
+				self:play_sound( "act" )
+			end
+
+			if visible then
 				if dist == 1 then
 					if math.random(100) <= self.attackchance then
 						self:fire( player, self.eq.weapon )
 					else
 						self:attack( player )
 					end
-					return "thinking"
+					self.irritated = 0
 				elseif dist < 4 then
 					self:fire( player, self.eq.weapon )
-					return "thinking"
+					self.irritated = 0
 				else
-					self.ai_state = "attack_spray"
-				end
-			else
-				if self.stun_time > 0 then
-					self.ai_state = "stagger"
-				else
-					self.ai_state = "pursue"
-				end
-			end
-
-
-			if not self.assigned then
-				local walk
-				local moves = {}
-				if self.ai_state == "pursue" then
-					if dist > self.vision then
-						self.move_to = player.position
-						self:path_find( self.move_to, 40, 200 )
-						self.assigned = true
-					else
-						for c in self.position:around_coords() do
-							if player:distance_to(c) == dist and generator.is_empty(c, { EF_NOBEINGS, EF_NOBLOCK } ) then
-								table.insert(moves,c:clone())
-							end
-						end
-						if #moves > 0 then
-							self.move_to = table.random_pick(moves)
-							self:path_find( self.move_to, 1, 1 ) --resets normal pathfind
+					local spray = area.around( player.position, math.floor(dist/3) )
+					local num_fire = self.eq.weapon.shots
+					self.eq.weapon.shots = 1
+					local animseq = 0
+					for shot = 1,num_fire do
+						local energy = self.scount
+						if math.random(2) == 1 then
+							self:fire( player, self.eq.weapon )
 						else
-							self.move_to = generator.random_empty_coord({ EF_NOBEINGS, EF_NOBLOCK }, area.around( self.position ))
-							self:path_find( self.move_to, 1, 1 ) --hopefully these settings don't make it expensive
+							local hit = spray:random_coord()
+							area.FULL:clamp_coord(hit)
+							self:fire( hit, self.eq.weapon )
 						end
+						if shot ~= 1 then
+							self.scount = energy
+						end
+						animseq = animseq + 60
+						self.animseq = animseq
 					end
-				elseif self.ai_state == "stagger" then
-					self.move_to = generator.random_empty_coord( { EF_NOBEINGS, EF_NOBLOCK }, area.around( self.position ) )
+					self.animseq = 0
+					self.eq.weapon.shots = num_fire
+				end
+				return "hunt"
+			end
+			self.irritated = self.irritated + 1
+
+			if dist > self.vision or self.irritated > 2 then
+                if self:path_find( player.position, 40, 200 ) and aitk.move_path( self ) then
+					return "hunt"
 				end
 			end
 
-			return self.ai_state
-		end,
-
-		attack_spray = function( self )
-			local dist = self:distance_to( player )
-			local spray = area.around( player.position, math.floor(dist/3) )
-			local num_fire = self.eq.weapon.shots
-			self.eq.weapon.shots = 1
-			for shot = 1,num_fire do
-				local energy = self.scount
-				if math.random(2) == 1 then
-					self:fire( player, self.eq.weapon )
-				else
-					local hit = spray:random_coord()
-					area.FULL:clamp_coord(hit)
-					self:fire( hit, self.eq.weapon )
+			local moves = {}
+			for c in self.position:around_coords() do
+				if player:distance_to(c) == dist and generator.is_empty(c, { EF_NOBEINGS, EF_NOBLOCK } ) then
+					table.insert(moves,c:clone())
 				end
-				if shot ~= 1 then
-					self.scount = energy
+			end
+			local best_moves = {}
+			for _,c in ipairs( moves ) do
+				if level:eye_contact( c, player ) then
+					table.insert( best_moves, c )
 				end
-				--ui.delay(missiles["mnat_mastermind"].delay * 2)
 			end
-			self.eq.weapon.shots = num_fire
-			return "thinking"
-		end,
-
-		stagger = function( self )
-			self:direct_seek( self.move_to )
-			self.stun_time = self.stun_time - 1
-			return "thinking"
-		end,
-
-		pursue = function( self )
-			if self:distance_to( self.move_to ) == 0 then
-				self.assigned = false
-				return "thinking"
+			if #best_moves > 0 then
+				moves = best_moves
 			end
-			if math.random(30) == 1 then
-				self:play_sound( "act" )
-			end
-			local move_check, move_coord
-			move_check,move_coord = self:path_next()
-
-			-- hack to prevent crash, think of something better later
-			if not move_check then
-				self.scount = self.scount - 100
+			if #moves == 0 then
+				move = generator.random_empty_coord({ EF_NOBEINGS, EF_NOBLOCK }, area.around( self.position ))
 			end
 
-			if not move_check or self:distance_to( player ) <= self.vision then
-				self.assigned = false
-			end
-			return "thinking"
+			local move = table.random_pick( moves )
+			if self:direct_seek( move ) ~= MOVEOK then
+                self.scount = self.scount - 1000
+            end
+			return "hunt"
 		end,
 	}
 }
