1f32e87dbb4d13e2c368e2653f9bc784c98153eb
brisbang <gareth.brisbane@outlook.com>
2025-06-24 23:39:17 +1000

    Empty flag now resets when a monster spawns in

--- src/dflevel.pas
+++ src/dflevel.pas
@@ -1438,6 +1438,7 @@ begin
       else iBeing := TBeing.Create( State.ToId(2) );
     Level.DropBeing( iBeing, State.ToCoord(3) );
     State.Push( iBeing );
+    Level.FEmpty := False;
   except
     on EPlacementException do
     begin
