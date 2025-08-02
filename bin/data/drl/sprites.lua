13a6784037d808493d23453ed8e472baf7faef4a
epyon <epyon@chaosforge.org>
2025-08-02 17:11:00 +0200

    fix for decal crash

--- bin/data/drl/sprites.lua
+++ bin/data/drl/sprites.lua
@@ -217,4 +217,5 @@ function drl.register_sprites()
 	core.declare( "HARDSPRITE_SELECT", SPRITE_SELECT )
 	core.declare( "HARDSPRITE_MARK",   SPRITE_MARK )
 	core.declare( "HARDSPRITE_GRID",   SPRITE_GRID )
+    core.declare( "HARDSPRITE_DECAL_BLOOD_1", 0 )
 end
