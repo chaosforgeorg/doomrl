803f58df22dba3fdb107098d3442ae6e404ac76b
epyon <epyon@chaosforge.org>
2025-08-09 20:28:10 +0200

    deploy support

---
+++ bin/deploy/drl/meta.lua
@@ -0,0 +1,12 @@
+meta = {
+  id            = "drl",
+  name          = "drl",
+  version       = "0.10.0",
+  base_required = "",
+  workshop_id   = "3545561236",
+  load_priority = 1000,
+  save_version  = 99900,
+  save_agnostic = false,
+  is_base       = true,
+}
+
